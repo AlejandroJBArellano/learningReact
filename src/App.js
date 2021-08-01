514d1c0a9f3fbdf90f812bb4a6d415ed2d231b57
AlejandroJBArellano <jesusarellano334@gmail.com>
2021-07-31 20:51:05 -0500

    Primer día aprendiendo React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hola = _ => <h1>Hola Mundo UwU</h1>,
+Logo = _ => <img src={logo} alt="Shut the fuck up"/>;
+
+class App extends React.Component {
+  render() {
+    return (
+      <section className="power">
+        <p>El mundo se ha convertido en la distopía de Orwell, por lo que todos imprimiremos</p>
+        <Hola />
+        <p>Y usaremos</p>
+        <Logo />
+      </section>
+    )
+  }
 }
+
+// function App() {
+
+// }
 
 export default App;
